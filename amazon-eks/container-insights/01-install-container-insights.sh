904b3e43c9f05e00cbcead71a89196548c34ccf8
Keshav <keshavkb@amazon.com>
2022-03-17 15:38:50 +0800

    Container insights

---
+++ amazon-eks/container-insights/01-install-container-insights.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+aws iam attach-role-policy \
+--role-name $NODE_ROLE_NAME \
+--policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy
+
+aws iam list-attached-role-policies \
+--role-name $NODE_ROLE_NAME \
+| grep CloudWatchAgentServerPolicy || echo 'Policy not found'
+
+
+ClusterName=$CLUSTER_NAME
+RegionName=$AWS_REGION
+FluentBitHttpPort='2020'
+FluentBitReadFromHead='Off'
+[[ ${FluentBitReadFromHead} = 'On' ]] && FluentBitReadFromTail='Off'|| FluentBitReadFromTail='On'
+[[ -z ${FluentBitHttpPort} ]] && FluentBitHttpServer='Off' || FluentBitHttpServer='On'
+curl https://raw.githubusercontent.com/aws-samples/amazon-cloudwatch-container-insights/latest/k8s-deployment-manifest-templates/deployment-mode/daemonset/container-insights-monitoring/quickstart/cwagent-fluent-bit-quickstart.yaml | sed 's/{{cluster_name}}/'${ClusterName}'/;s/{{region_name}}/'${RegionName}'/;s/{{http_server_toggle}}/"'${FluentBitHttpServer}'"/;s/{{http_server_port}}/"'${FluentBitHttpPort}'"/;s/{{read_from_head}}/"'${FluentBitReadFromHead}'"/;s/{{read_from_tail}}/"'${FluentBitReadFromTail}'"/' | kubectl apply -f -
+echo "Installed Container Insights"
+ 
