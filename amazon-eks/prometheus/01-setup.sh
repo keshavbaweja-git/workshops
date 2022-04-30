f0870c84bef0eacb0b94969d34d3a0d2be4c53e1
Keshav <keshavkb@amazon.com>
2022-04-30 20:28:28 +0800

    EKS Prom

--- amazon-eks/prometheus/01-setup.sh
+++ amazon-eks/prometheus/01-setup.sh
@@ -1,5 +1,5 @@
 ##!/bin/bash
-CLUSTER_NAME=mycluste1
+CLUSTER_NAME=mycluster1
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 OIDC_PROVIDER=$(aws eks describe-cluster --name $CLUSTER_NAME --query "cluster.identity.oidc.issuer" --output text | sed -e "s/^https:\/\///")
 
@@ -11,38 +11,15 @@ SERVICE_ACCOUNT_IAM_ROLE_DESCRIPTION="IAM role to be used by a K8s service accou
 SERVICE_ACCOUNT_IAM_POLICY=AWSManagedPrometheusWriteAccessPolicy
 SERVICE_ACCOUNT_IAM_POLICY_ARN=arn:aws:iam::$AWS_ACCOUNT_ID:policy/$SERVICE_ACCOUNT_IAM_POLICY
 
-#
-# Create an IAM permission policy to be associated with the role, if the policy does not already exist
-#
-SERVICE_ACCOUNT_IAM_POLICY_ID=$(aws iam get-policy --policy-arn $SERVICE_ACCOUNT_IAM_POLICY_ARN --query 'Policy.PolicyId' --output text)
-if [ "$SERVICE_ACCOUNT_IAM_POLICY_ID" = "" ]; 
-then
-  echo "Creating a new permission policy $SERVICE_ACCOUNT_IAM_POLICY"
-  aws iam create-policy --policy-name $SERVICE_ACCOUNT_IAM_POLICY --policy-document file://PermissionPolicy.json 
-else
-  echo "Permission policy $SERVICE_ACCOUNT_IAM_POLICY already exists"
-fi
+aws iam create-policy --policy-name $SERVICE_ACCOUNT_IAM_POLICY --policy-document file://permission-policy.json 
 
-#
-# If the IAM role already exists, then just update the trust policy.
-# Otherwise create one using the trust policy and permission policy
-#
-SERVICE_ACCOUNT_IAM_ROLE_ARN=$(aws iam get-role --role-name $SERVICE_ACCOUNT_IAM_ROLE --query 'Role.Arn' --output text)
-if [ "$SERVICE_ACCOUNT_IAM_ROLE_ARN" = "" ]; 
-then
-  echo "$SERVICE_ACCOUNT_IAM_ROLE role does not exist. Creating a new role with a trust and permission policy"
 
-  SERVICE_ACCOUNT_IAM_ROLE_ARN=$(aws iam create-role \
-  --role-name $SERVICE_ACCOUNT_IAM_ROLE \
-  --assume-role-policy-document file://TrustPolicy.json \
-  --description "$SERVICE_ACCOUNT_IAM_ROLE_DESCRIPTION" \
-  --query "Role.Arn" --output text)
+SERVICE_ACCOUNT_IAM_ROLE_ARN=$(aws iam create-role \
+--role-name $SERVICE_ACCOUNT_IAM_ROLE \
+--assume-role-policy-document file://trust-policy.json \
+--description "$SERVICE_ACCOUNT_IAM_ROLE_DESCRIPTION" \
+--query "Role.Arn" --output text)
 
-  aws iam attach-role-policy --role-name $SERVICE_ACCOUNT_IAM_ROLE --policy-arn $SERVICE_ACCOUNT_IAM_POLICY_ARN  
-else
-  echo "$SERVICE_ACCOUNT_IAM_ROLE_ARN role already exists. Updating the trust policy"
-
-  aws iam update-assume-role-policy --role-name $SERVICE_ACCOUNT_IAM_ROLE --policy-document file://TrustPolicy.json
-fi
+aws iam attach-role-policy --role-name $SERVICE_ACCOUNT_IAM_ROLE --policy-arn $SERVICE_ACCOUNT_IAM_POLICY_ARN  
 echo $SERVICE_ACCOUNT_IAM_ROLE_ARN
 
