ce2ed16b450c7ff458d2e81264e356c0b18f6868
Keshav <keshavkb@amazon.com>
2022-04-30 20:02:07 +0800

    EKS Prometheus

---
+++ amazon-eks/prometheus/01-setup.sh
@@ -0,0 +1,48 @@
+##!/bin/bash
+CLUSTER_NAME=mycluste1
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+OIDC_PROVIDER=$(aws eks describe-cluster --name $CLUSTER_NAME --query "cluster.identity.oidc.issuer" --output text | sed -e "s/^https:\/\///")
+
+PROM_SERVICE_ACCOUNT_NAMESPACE=prometheus
+GRAFANA_SERVICE_ACCOUNT_NAMESPACE=grafana
+SERVICE_ACCOUNT_NAME=iamproxy-service-account
+SERVICE_ACCOUNT_IAM_ROLE=EKS-AMP-ServiceAccount-Role
+SERVICE_ACCOUNT_IAM_ROLE_DESCRIPTION="IAM role to be used by a K8s service account with write access to AMP"
+SERVICE_ACCOUNT_IAM_POLICY=AWSManagedPrometheusWriteAccessPolicy
+SERVICE_ACCOUNT_IAM_POLICY_ARN=arn:aws:iam::$AWS_ACCOUNT_ID:policy/$SERVICE_ACCOUNT_IAM_POLICY
+
+#
+# Create an IAM permission policy to be associated with the role, if the policy does not already exist
+#
+SERVICE_ACCOUNT_IAM_POLICY_ID=$(aws iam get-policy --policy-arn $SERVICE_ACCOUNT_IAM_POLICY_ARN --query 'Policy.PolicyId' --output text)
+if [ "$SERVICE_ACCOUNT_IAM_POLICY_ID" = "" ]; 
+then
+  echo "Creating a new permission policy $SERVICE_ACCOUNT_IAM_POLICY"
+  aws iam create-policy --policy-name $SERVICE_ACCOUNT_IAM_POLICY --policy-document file://PermissionPolicy.json 
+else
+  echo "Permission policy $SERVICE_ACCOUNT_IAM_POLICY already exists"
+fi
+
+#
+# If the IAM role already exists, then just update the trust policy.
+# Otherwise create one using the trust policy and permission policy
+#
+SERVICE_ACCOUNT_IAM_ROLE_ARN=$(aws iam get-role --role-name $SERVICE_ACCOUNT_IAM_ROLE --query 'Role.Arn' --output text)
+if [ "$SERVICE_ACCOUNT_IAM_ROLE_ARN" = "" ]; 
+then
+  echo "$SERVICE_ACCOUNT_IAM_ROLE role does not exist. Creating a new role with a trust and permission policy"
+
+  SERVICE_ACCOUNT_IAM_ROLE_ARN=$(aws iam create-role \
+  --role-name $SERVICE_ACCOUNT_IAM_ROLE \
+  --assume-role-policy-document file://TrustPolicy.json \
+  --description "$SERVICE_ACCOUNT_IAM_ROLE_DESCRIPTION" \
+  --query "Role.Arn" --output text)
+
+  aws iam attach-role-policy --role-name $SERVICE_ACCOUNT_IAM_ROLE --policy-arn $SERVICE_ACCOUNT_IAM_POLICY_ARN  
+else
+  echo "$SERVICE_ACCOUNT_IAM_ROLE_ARN role already exists. Updating the trust policy"
+
+  aws iam update-assume-role-policy --role-name $SERVICE_ACCOUNT_IAM_ROLE --policy-document file://TrustPolicy.json
+fi
+echo $SERVICE_ACCOUNT_IAM_ROLE_ARN
+
---
+++ amazon-eks/prometheus/install.sh
@@ -0,0 +1,6 @@
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+kubectl create ns prometheus
+helm install prometheus-for-amp prometheus-community/prometheus -n prometheus -f ./amp_ingest_override_values.yaml \
+--set serviceAccounts.server.annotations."eks\.amazonaws\.com/role-arn"="${IAM_PROXY_PROMETHEUS_ROLE_ARN}" \
+--set server.remoteWrite[0].url="https://aps-workspaces.${AWS_REGION}.amazonaws.com/workspaces/${WORKSPACE_ID}/api/v1/remote_write" \
+--set server.remoteWrite[0].sigv4.region=${AWS_REGION}
