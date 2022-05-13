d066cae1f5a5e070b9f5a2f64cdbaf120576a331
Keshav <keshavkb@amazon.com>
2022-05-13 22:47:10 +0800

    Apache Kafka

---
+++ apache-kafka/04-configure-tls.sh
@@ -0,0 +1,84 @@
+### Create working directory
+mkdir -p /home/ec2-user/kafka-certs
+cd /home/ec2-user/kafka-certs
+
+
+password=password
+hostname=$(hostname)
+### Create CA certificate
+echo 'Creating: CA certificate'
+openssl req -new -x509 \
+-keyout ca-key -out ca-cert \
+-days 365 -passin pass:$password \
+-subj "/C=SG/O=AWS/OU=SA/CN=ca"
+echo 'Created: CA certificate'
+
+### Create server truststore
+echo 'Creating: server truststore'
+keytool -keystore kafka.server.truststore.jks \
+-alias CARoot -importcert -file ca-cert \
+-storepass $password -noprompt
+echo 'Created: server truststore'
+
+### Create client truststore
+echo 'Creating: client truststore'
+keytool -keystore kafka.client.truststore.jks \
+-alias CARoot -importcert -file ca-cert \
+-storepass $password -noprompt
+echo 'Created: client truststore'
+
+### Create server keystore
+echo 'Creating: server keystore'
+keytool -keystore kafka.server.keystore.jks \
+-alias localhost -keyalg RSA \
+-validity 365 -genkey \
+-storepass $password -keypass $password \
+-dname "CN=server01, OU=SA, O=AWS, L=Singapore, ST=Singapore, C=SG" \
+-ext SAN=DNS:$hostname
+echo 'Created: server keystore'
+
+echo 'Creating, adding: server cert to server keystore'
+keytool -keystore kafka.server.keystore.jks \
+-alias localhost -certreq -file cert-file \
+-storepass $password
+
+openssl x509 -req -CA ca-cert -CAkey ca-key \
+-in cert-file -out cert-signed \
+-days 365 -CAcreateserial -passin pass:$password
+
+keytool -keystore kafka.server.keystore.jks \
+-alias CARoot -importcert -file ca-cert \
+-storepass $password -noprompt
+
+keytool -keystore kafka.server.keystore.jks \
+-alias localhost -importcert -file cert-signed \
+-storepass $password -noprompt
+echo 'Created, added: server cert to server keystore'
+
+### Create client keystore
+echo 'Creating: client keystore'
+keytool -keystore kafka.client.keystore.jks \
+-alias localhost -keyalg RSA \
+-validity 365 -genkey \
+-storepass $password -keypass $password \
+-dname "CN=client01, OU=SA, O=AWS, L=Singapore, ST=Singapore, C=SG"
+echo 'Created: client keystore'
+
+echo 'Creating, adding: client cert to client keystore'
+keytool -keystore kafka.client.keystore.jks \
+-alias localhost -certreq -file cert-file-client \
+-storepass $password
+
+openssl x509 -req -CA ca-cert -CAkey ca-key \
+-in cert-file-client -out cert-signed-client \
+-days 365 -CAcreateserial -passin pass:password
+
+keytool -keystore kafka.client.keystore.jks \
+-alias CARoot -importcert -file ca-cert \
+-storepass $password -noprompt
+
+keytool -keystore kafka.client.keystore.jks \
+-alias localhost -importcert -file cert-signed-client \
+-storepass $password -noprompt
+echo 'Created, added: client cert to client keystore'
+
---
+++ apache-kafka/05-configure-tls.md
@@ -0,0 +1,29 @@
+1. Update server properties
+```
+# Add following lines to $KAFKA_HOME/config/server.properties
+ssl.truststore.location=/home/ec2-user/kafka-certs/kafka.server.truststore.jks
+ssl.truststore.password=password
+ssl.keystore.location=/home/ec2-user/kafka-certs/kafka.server.keystore.jks
+ssl.keystore.password=password
+ssl.key.password=password
+```
+
+2. Create client properties
+```
+### Add following lines to /home/ec2-user/kafka-certs/client-ssl.properties
+bootstrap.servers=localhost:9093
+security.protocol=SSL
+ssl.endpoint.identification.algorithm=
+ssl.truststore.location=/home/ec2-user/kafka-certs/kafka.client.truststore.jks
+ssl.truststore.password=password
+ssl.keystore.location=/home/ec2-user/kafka-certs/kafka.client.keystore.jks
+ssl.keystore.password=password
+ssl.key.password=password
+```
+
+3. Restart Kafka server
+
+4. Consume messages using SSL
+```
+$KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server localhost:9093 --topic quickstart-events --consumer.config /home/ec2-user/kafka-certs/client-ssl.properties --from-beginning
+```
