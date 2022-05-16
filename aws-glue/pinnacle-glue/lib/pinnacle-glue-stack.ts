4755a9335d0b88a4de17616314fd9f548116f158
Keshav <keshavkb@amazon.com>
2022-05-16 12:15:52 +0800

    AWS Glue

--- aws-glue/pinnacle-glue/lib/pinnacle-glue-stack.ts
+++ aws-glue/pinnacle-glue/lib/pinnacle-glue-stack.ts
@@ -1,7 +1,11 @@
 import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
-import { Construct } from "constructs";
+import {
+  GatewayVpcEndpoint,
+  GatewayVpcEndpointAwsService, Vpc
+} from "aws-cdk-lib/aws-ec2";
 import * as iam from "aws-cdk-lib/aws-iam";
 import { ManagedPolicy, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
 
 export class PinnacleGlueStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -11,7 +15,9 @@ export class PinnacleGlueStack extends Stack {
       assumedBy: new ServicePrincipal("glue.amazonaws.com"),
       roleName: "AWSGlueServiceRoleDefault",
       managedPolicies: [
-        ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSGlueServiceRole"),
+        ManagedPolicy.fromAwsManagedPolicyName(
+          "service-role/AWSGlueServiceRole"
+        ),
         ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess"),
       ],
     });
@@ -19,6 +25,12 @@ export class PinnacleGlueStack extends Stack {
     new CfnOutput(this, "GlueServiceRoleArn", {
       exportName: "GlueServiceRoleArn",
       value: glueServiceRole.roleArn,
+    });
+
+    const vpc = Vpc.fromLookup(this, "VPC", { vpcId: "vpc-02eaa7bdb3bf1573c" });
+    new GatewayVpcEndpoint(this, "S3GatewayVpcEndpoint", {
+      vpc,
+      service: GatewayVpcEndpointAwsService.S3,
     });
   }
 }
