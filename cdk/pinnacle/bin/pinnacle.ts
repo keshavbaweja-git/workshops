79799b0817096873d7b6f525ae473a430d2675c0
Keshav <keshavkb@amazon.com>
2022-07-31 21:01:37 +0800

    Vpc, IAM stack

--- cdk/pinnacle/bin/pinnacle.ts
+++ cdk/pinnacle/bin/pinnacle.ts
@@ -2,9 +2,16 @@
 import * as cdk from "aws-cdk-lib";
 import "source-map-support/register";
 import { PinnacleIamStack } from "../lib/pinnacle-iam-stack";
+import { PinnacleVpcStack } from "../lib/pinnacle-vpc-stack";
 
 const app = new cdk.App();
 new PinnacleIamStack(app, "PinnacleIamStack", {
+  env: {
+    account: process.env.CDK_ACCOUNT_ID,
+    region: process.env.CDK_REGION_ID,
+  },
+});
+new PinnacleVpcStack(app, "PinnacleVpcStack", {
   env: {
     account: process.env.CDK_ACCOUNT_ID,
     region: process.env.CDK_REGION_ID,
--- cdk/pinnacle/lib/pinnacle-vpc-stack.ts
+++ cdk/pinnacle/lib/pinnacle-vpc-stack.ts
@@ -6,13 +6,13 @@ export class PinnacleVpcStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const pinnacleVpc = new ec2.Vpc(this, "pinnacleVpc", {
-      cidr: "10.10.0.0/16",
-      vpcName: "pinnacle-us-east-1",
+    const pinnacleVpc = new ec2.Vpc(this, "PinnacleVpc", {
+      cidr: "10.1.0.0/16",
+      vpcName: "pinnacle",
     });
 
-    new CfnOutput(this, "vpcOnPremisesId", {
-      exportName: "VpcOnPremisesId",
+    new CfnOutput(this, "PinnacleVpcId", {
+      exportName: "PinnacleVpcId",
       value: pinnacleVpc.vpcId,
     });
   }
