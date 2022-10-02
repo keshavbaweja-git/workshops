7bf5893d43fedce3207c8ac6b052a720e49aeaf1
Keshav <keshavkb@amazon.com>
2022-10-02 19:22:19 +0800

    EKS xray

---
+++ amazon-eks/xray/install-sample-app.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://eksworkshop.com/intermediate/245_x-ray/sample-front.files/x-ray-sample-front-k8s.yml
+kubectl apply -f https://eksworkshop.com/intermediate/245_x-ray/sample-back.files/x-ray-sample-back-k8s.yml
