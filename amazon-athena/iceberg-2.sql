7a5b4b76d76f95cd45ef1b21c0c85cbf78d939a2
Keshav <keshavkb@amazon.com>
2022-11-05 12:38:18 +0800

    Amazon Athena

--- amazon-athena/iceberg-2.sql
+++ amazon-athena/iceberg-2.sql
@@ -11,8 +11,17 @@ values (1, 'row1', 'A', date '2022-10-12')
 insert into iceberg_table (id, data, category, cob_date)
 values (2, 'row2', 'B', date '2022-10-13')
 
-select * from iceberg_table;
+insert into iceberg_table (id, data, category, cob_date)
+values (3, 'row3', 'C', date '2022-10-14')
+
+select * from iceberg_table
+
+update iceberg_table set data='row22' where id=2
+
+select * from iceberg_table for system_time as of timestamp '2022-11-30 23:59:59'
 
 drop table iceberg_table
 
 drop database iceberg_database
+
+select current_date, current_time
