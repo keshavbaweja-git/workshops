4968e900d6ad56e38dbd1e8fadaf995f2d0238fb
Keshav <keshavkb@amazon.com>
2022-12-29 13:45:53 +0800

    App Mesh

--- amazon-eks/app-mesh/01-install-app-mesh.sh
+++ amazon-eks/app-mesh/01-install-app-mesh.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
+# Check required environment variables are set
+if [[ -z "$CLUSTER_NAME" ]]; then
+    echo "Exiting, please set CLUSTER_NAME environment variable" 1>&2
+    exit 1
+fi
+
+if [[ -z "$AWS_REGION" ]]; then
+    echo "Exiting, please set AWS_REGION environment variable" 1>&2
+    exit 1
+fi
+
+set -ex
+
+# Install App Mesh CRDs
 kubectl apply -k "https://github.com/aws/eks-charts/stable/appmesh-controller/crds?ref=master"
 echo "Installed appmesh-controller CRDs"
 
+# List api-resources for App Mesh
+kubectl api-resources --api-group=appmesh.k8s.aws
+
+# Create App Mesh namespace
 kubectl create ns appmesh-system
 echo "Created namespace for appmesh-controller"
 
+# Create App Mesh IRSA (IAM role for ServiceAccount)
 eksctl create iamserviceaccount \
 --cluster $CLUSTER_NAME \
 --namespace appmesh-system \
@@ -15,6 +34,7 @@ eksctl create iamserviceaccount \
 --approve
 echo "Created serviceAccount with IAM role for appmesh-controller"
 
+# Install App Mesh contoller
 helm repo add eks https://aws.github.io/eks-charts
 
 helm upgrade -i appmesh-controller eks/appmesh-controller \
