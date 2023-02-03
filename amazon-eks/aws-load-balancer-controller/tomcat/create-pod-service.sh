e4c26f3d35470047369cd8cbea7a0358641e0735
Keshav <keshavkb@amazon.com>
2023-02-03 13:34:06 +0800

    Glue Spark Iceberg

--- amazon-eks/aws-load-balancer-controller/tomcat/create-pod-service.sh
+++ amazon-eks/aws-load-balancer-controller/tomcat/create-pod-service.sh
@@ -1,1 +1,6 @@
 k run my-tomcat --image=tomcat:8.5-jdk8-corretto --expose --port=8080
+
+k create secret generic my-secret --from-literal=key1=supersecret2 --from-literal=key2=topsecret2
+
+k get secret my-secret -o json | jq -r .data.key1 | base64 -d
+k exec -ti my-tomcat-6455f5c94c-52bjg -- cat /var/run/secrets/my-tomcat/key1
